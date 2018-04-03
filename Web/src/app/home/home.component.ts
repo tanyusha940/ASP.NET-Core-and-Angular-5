6bcf8295a2a4dd4c6d34a0914f95aed02568c8e4
Unknown <tanyusha_shkoda@mail.ru>
2018-04-03 17:13:20 +0300

    added cloude tegs but not works async

--- Web/src/app/home/home.component.ts
+++ Web/src/app/home/home.component.ts
@@ -20,68 +20,34 @@ export class HomeComponent implements OnInit {
   form: FormGroup;
   conspectItems: ConspectItem[];
   tagItems: TagItem[];
-  words: Array<AgWordCloudData>;
+  wordData:CloudData[] =[];
 
   constructor(
     private conspectsService: ConspectsService,
     private tagsService: TagsService
-  ) { }
+  ) {   }
   public random(): number{
-      return Math.floor(Math.random() * 1000);
+      return Math.floor(Math.random() * 10);
   }
 
-  
   async ngOnInit() {
-    this.conspectItems = await this.conspectsService.getConspects();   
-    console.log(this.conspectItems);   
-    this.words = await this.getWords();
-  }
-
-  async getWords(): Promise<Array<AgWordCloudData>> {
+    this.conspectItems = await this.conspectsService.getConspects();  
     this.tagItems = await this.tagsService.getTags();
-    let wordData: Array<AgWordCloudData> = [];
-    this.tagItems.forEach((tag:any) => {
-      wordData.push(<AgWordCloudData>{
-        size: 250,
-        text: tag.text
-      });
-    });
-    
-    console.log(wordData);
-    return wordData;
+    console.log(this.getWords());
   }
 
-  options = {
-    settings: {
-        minFontSize: 10,
-        maxFontSize: 100,
-    },
-    margin: {
-        top: 10,
-        right: 10,
-        bottom: 10,
-        left: 10
-    },
-    labels: true // false to hide hover labels
-};
-
-  // options: CloudOptions = {
-  //   // if width is between 0 and 1 it will be set to the size of the upper element multiplied by the value 
-  //   width : 1000,
-  //   height : 400,
-  //   overflow: false,
-  // }
- 
-  // data: CloudData[] = [
-  //   {text: 'Weight-8-link-color', weight: 8, color: '#ffaaee'},
-  //   {text: 'Weight-10-link', weight: 5},
-  //   {text: 'Weight-10-link', weight: 17},
-  //   {text: 'яыпink', weight: 14},
-  //   {text: 'Weighяырыяп-link', weight: 10},
-  //   {text: 'Weчвкр-link', weight: 8},
-  //   {text: 'Weighявпыt-10-link', weight: 10},
-  //   {text: 'Weight-10-link', weight: 2},
-  //   {text: 'Weighыаяlink', weight: 7},
-  //   // ...
-  // ]
+  options: CloudOptions = {
+    width : 1000,
+    height : 400,
+    overflow: false,
+  }
+ async getWords(){
+  this.tagItems.forEach(tag => {
+    this.wordData.push({
+            weight: this.random(),
+            text: tag.text
+          });
+  });
+  return await this.wordData;
+  }
 }
