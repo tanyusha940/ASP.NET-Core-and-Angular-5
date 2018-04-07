138f57db1af32b08b606a1bc5189dc7961670a9d
Unknown <tanyusha_shkoda@mail.ru>
2018-04-07 13:20:12 +0300

    create data localization on admin page

--- Web/src/app/admin-page/admin/admin.component.ts
+++ Web/src/app/admin-page/admin/admin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersItem } from '@app/admin-page/admin/models/userItem';
 import { UsersService } from '@app/admin-page/admin/users.service';
+import { I18nService } from '@app/core';
 
 @Component({
   selector: 'app-admin',
@@ -11,11 +12,22 @@ export class AdminComponent implements OnInit {
 
   userItem: UsersItem[];
   constructor(
-    private userService: UsersService
+    private userService: UsersService,
+    private i18nService: I18nService
   ) { }
 
   async ngOnInit() {
     this.userItem = await this.userService.getUsers();
   }
 
+  setLanguage(language: string) {
+    this.i18nService.language = language;
+  }
+
+  get currentLanguage(): string {
+    return this.i18nService.language;
+  }
+  get languages(): string[] {
+    return this.i18nService.supportedLanguages;
+  }
 }
