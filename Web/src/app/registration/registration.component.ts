8e72d9dbf0e366b827b88b23810cb3f2b7bf8ae3
Unknown <tanyusha_shkoda@mail.ru>
2018-04-07 18:26:38 +0300

    fixed registration form

--- Web/src/app/registration/registration.component.ts
+++ Web/src/app/registration/registration.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { I18nService } from '@app/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -25,12 +25,67 @@ export class RegistrationComponent implements OnInit {
   private initForm() {
     this.form = this.fb.group({
       UserName: ['', [
+        Validators.required
       ]],
       Email: ['', [
+        Validators.required,
+        Validators.email
       ]],
       Password: ['', [
+        Validators.required,
+        Validators.pattern(/[a-zA-Z0-9]/),
+        Validators.min(6)
+      ]],
+      RepeatPassword:['',[
+        Validators.required,
+        this.matchOtherValidator('Password')
       ]]
     });
+  }
+
+  matchOtherValidator (otherControlName: string) {
+
+    let thisControl: FormControl;
+    let otherControl: FormControl;
+  
+    return function matchOtherValidate (control: FormControl) {
+  
+      if (!control.parent) {
+        return null;
+      }
+  
+      // Initializing the validator.
+      if (!thisControl) {
+        thisControl = control;
+        otherControl = control.parent.get(otherControlName) as FormControl;
+        if (!otherControl) {
+          throw new Error('matchOtherValidator(): other control is not found in parent group');
+        }
+        otherControl.valueChanges.subscribe(() => {
+          thisControl.updateValueAndValidity();
+        });
+      }
+  
+      if (!otherControl) {
+        return null;
+      }
+  
+      if (otherControl.value !== thisControl.value) {
+        return {
+          matchOther: true
+        };
+      }
+  
+      return null;
+  
+    }
+  
+  }
+
+  isControlInvalid(controlName: string): boolean {
+    const control = this.form.controls[controlName];
+    const result = control.invalid && control.touched;
+    return result;
   }
 
   setLanguage(language: string) {
